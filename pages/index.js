d880da34e556e86a4749c8649c2d6a75b6642184
SilkyRoad-marketplace <andytanth82@gmail.com>
2025-11-09 10:07:42 +0800

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-
-export default function Home() {
-  return <div style={{textAlign:'center', marginTop:'100px'}}>
-    <h1>Welcome to SilkyRoad Marketplace</h1>
-    <p>Next.js is configured correctly ✅</p>
-  </div>;
-}
